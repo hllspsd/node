c3611a673c98215b9d892258b06b876f4075c506
hllspsd <513104331@qq.com>
2016-05-11 11:59:48 +0800

    new add

--- example.js
+++ example.js
@@ -1,14 +1,36 @@
 const http = require('http');
+const path = require('path');
+const express = require('express');
+const router = require('./routes/index');
+const favicon = require('serve-favicon');//设置favicon
+const logger = require('morgan');//打印的nodejs 服务器接受到的请求的信息
+
+const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const [port = 3000] = [process.env.PORT];
 
-const server = http.createServer((req, res) => {
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', port);
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(logger('dev'));
+
+app.get('/', router);
+
+/*const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Hello World\n');
-})
+})*/
 
-server.listen(port, hostname, () => {
+const server = http.createServer(app);
+
+server.listen(port);
+
+/*server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
-})
+})*/
+
+console.log(process.env.PORT || 3000);
