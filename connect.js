82492c3f8e53b2a3100c303dadf944fbd67e2188
hllspsd <513104331@qq.com>
2016-05-13 18:11:09 +0800

    new add 5.13

--- connect.js
+++ connect.js
@@ -1,8 +1,11 @@
 const connect = require('connect');
+const parse = require('url').parse;
+const cookieParser = require('cookie-parser');
 
 var app = connect();
 
 app.use('/admin', hello);//挂载
+app.use(connect.cookieParser('213'));
 app.use(aa(123));
 app.use(bb);
 
@@ -10,16 +13,22 @@ app.listen(3000);
 
 function hello(req, res, next) {
 	console.log('%s %s', req.url, req.method);
+	if(req.url == '/') {
+		console.log(req.cookies);
+		//console.log(parse(req.url));
+		res.end(parse(req.url).toString());
+		return;
+	}
 	next();
 }
 
 function aa(options) {
 	return function(req, res, next) {
-		console.log(options);
+		//console.log(parse(req.url));
 		next();
 	}
 }
 
 function bb(req, res, next) {
-	res.end('123');
+	res.end('1234');
 }
